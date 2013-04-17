465eca94924f629070d0fce61c4854e2d79fee3e
Greg Hetrick <greghetrick@gmail.com>
2013-04-18 00:45:37 +0400

    Added usage for client secret and developer key outside of the script in client_secrets.json file This will allow for publishing of the script for others

--- gbackup.py
+++ gbackup.py
@@ -26,13 +26,11 @@ import mimetypes
 
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
-from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 from apiclient import errors
 from oauth2client.file import Storage
 
-CLIENT_ID = '303911097105.apps.googleusercontent.com'
-CLIENT_SECRET = 'GWLv1F-cFwDn4PxOQnY9v1mL'
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
@@ -77,7 +75,7 @@ for opt,toss in opts:
                 else:
                         storage = Storage(credfile)
                         creds = storage.get()
-                        flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+                        flow = flow_from_clientsecrets('client_secrets.json', OAUTH_SCOPE, REDIRECT_URI)
 
                         #Create an httplib2.Http object and authorize it with our credentials
                         http = httplib2.Http()
@@ -103,9 +101,9 @@ for opt,toss in opts:
                                         status, response = file.next_chunk()
                                         if status:
                                                 print "Uploaded %d%%." % int(status.progress() *100)
-                                print "Upload Complete!"
+                                print filename + " - Upload Complete!"
         elif opt in ("-i", "--init"):
-                flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+                flow = flow_from_clientsecrets('client_secrets.json', OAUTH_SCOPE, REDIRECT_URI)
                 authorize_url = flow.step1_get_authorize_url()
                 print 'Go to the following link in your browser: \n' + authorize_url
                 code = raw_input('Enter verification code: ').strip()
